c3d8e5ad5982101e03292bd370c470b05ca5f728
Fikus00 <filipnikolic0000@gmail.com>
2023-05-16 00:08:01 +0200

    adds cors

--- api-gateway/startup/server.go
+++ api-gateway/startup/server.go
@@ -4,10 +4,12 @@ import (
 	cfg "api-gateway/startup/config"
 	"context"
 	"fmt"
+	"github.com/spf13/viper"
 	"log"
 	"net/http"
+	"regexp"
 
-	reservationGw "github.com/XWS-tim24/Common/common/proto/accommodation_reservation_service"
+	reservationGw "github.com/XWS-tim24/Common/common/proto/accommodation_reservation_service" //27107, 5000, 3000
 	accommodationGw "github.com/XWS-tim24/Common/common/proto/accommodation_service"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
@@ -46,5 +48,29 @@ func (server *Server) initHandlers() {
 }
 
 func (server *Server) Start() {
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", server.config.Port), server.mux))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", server.config.Port), cors(server.mux)))
+}
+
+func allowedOrigin(origin string) bool {
+	if viper.GetString("cors") == "*" {
+		return true
+	}
+	if matched, _ := regexp.MatchString(viper.GetString("cors"), origin); matched {
+		return true
+	}
+	return false
+}
+
+func cors(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if allowedOrigin(r.Header.Get("Origin")) {
+			w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization, ResponseType")
+		}
+		if r.Method == "OPTIONS" {
+			return
+		}
+		h.ServeHTTP(w, r)
+	})
 }
